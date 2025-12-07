96532f8b3ab872b9858c7a031ef2c9731ececdaf
rewky05 <apur.rosales.swu@phinmaed.com>
2025-12-07 20:47:44 +0800

    updated layout str for admin

--- components/ui/admin/appointments/index.ts
+++ components/ui/admin/appointments/index.ts
@@ -1,8 +1,10 @@
-export { default as AppointmentsHeader } from './AppointmentsHeader';
-export { default as AppointmentsTabs } from './AppointmentsTabs';
-export { default as AppointmentsSearchBar } from './AppointmentsSearchBar';
-export { default as AppointmentsList } from './AppointmentsList';
 export { default as AppointmentCard } from './AppointmentCard';
+export { default as AppointmentsHeader } from './AppointmentsHeader';
+export { default as AppointmentsList } from './AppointmentsList';
+export { default as AppointmentsSearchBar } from './AppointmentsSearchBar';
+export { default as AppointmentsTabs } from './AppointmentsTabs';
 export { default as CancelReasonModal } from './CancelReasonModal';
 export type { CancelReason } from './CancelReasonModal';
+export { default as CompleteConfirmationModal } from './CompleteConfirmationModal';
+export { default as SuccessModal } from './SuccessModal';
 
